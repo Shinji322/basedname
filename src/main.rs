a97ee06aedd573d1ba15a0a1f8b499be4fdf47c2
Shinji322 <ronananakinmaharaj@gmail.com>
2022-06-08 12:44:21 -0400

    There is no need for a Result enum since a string is always returned by truncate

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use std::env::args;
 use std::process::exit;
 pub mod lib;
 pub mod cli;
-use lib::truncate;
 use cli::Args;
 
-pub fn run(args: Args) -> Result<String, Box<dyn std::error::Error>> {
-    Ok(truncate(args.query, args.matches))
+pub fn run(args: Args) -> String {
+    lib::truncate(args.query, args.matches)
 }
 
 fn main() {
@@ -14,7 +13,7 @@ fn main() {
     if argc == 0 {
         match Args::stdin() {
             Ok(stdin) => {
-                println!("{}", run(stdin).unwrap());
+                println!("{}", run(stdin));
                 exit(0);
             },
             Err(e) => {
@@ -24,28 +23,12 @@ fn main() {
         }
     } else if argc == 1 {
         let arg = args().skip(1).collect();
-        match run(Args::new(arg, 0)) {
-            Ok(val) => {
-                println!("{}", val);
-                exit(0);
-            },
-            Err(_) =>  {
-                // Never happens, but maybe for later
-                exit(1);
-            }
-        }
+        println!("{}", run(Args::new(arg, 1)));
+        exit(0);
     }
 
     // Otherwise, the code is running with multiple arguments and that should be handled by clap
     let args = Args::arg_parse();
-    match run(args) {
-        Ok(val) => {
-            println!("{}", val);
-        },
-        Err(_) =>  {
-            // Never happens, but maybe for later
-            exit(1);
-        }
-    }
+    println!("{}", run(args));
     exit(0);
 }
